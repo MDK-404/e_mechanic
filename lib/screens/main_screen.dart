124ac0731732d3b31c105621d43a00afb8f47d0f
Muhammad Daniyal Khan <mdk968196@gmail.com>
2024-04-28 10:52:46 +0500

    first commit

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        // AppBar mein decoration set karna
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.orange, // Border color set karna
+                width: 2.0,
+              ),
+              // Border ki width set karna
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black
+                    .withOpacity(0.5), // Shadow ka color aur opacity
+                spreadRadius: 4, // Shadow ka spread radius
+                blurRadius: 8, // Shadow ka blur radius
+                offset: Offset(0, 4), // Shadow ki position set karna
+              ),
+            ],
+          ),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'E',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Poppins',
+                      color: Colors.orange, // E ko orange color set karna
+                    ),
+                  ),
+                  TextSpan(
+                    text: '-Mechanic',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Poppins',
+                      color:
+                          Colors.white, // Baaki text ko white color set karna
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 10),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 40,
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 50),
+            // Customer section container
+            Container(
+              height: 250,
+              margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/mechanic.png',
+                    width: 250,
+                    height: 100,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/customerLogin');
+                    },
+                    child: Text(
+                      'Customer Login',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 50),
+            // Mechanic section container
+            Container(
+              height: 250,
+              margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/mechanic.png',
+                    width: 250,
+                    height: 100,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/mechanicLogin');
+                    },
+                    child: Text(
+                      'Mechanic Login',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Background image
+            Image.asset(
+              'assets/images/logo.png',
+              fit: BoxFit.contain,
+              // width: double.infinity,
+            ),
+            // SizedBox for spacing
+            SizedBox(height: 10),
+            // Welcome text
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.orange,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            // SizedBox for spacing
+            SizedBox(height: 10),
+            // Let's get started text button
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainScreen()),
+                );
+              },
+              child: Text(
+                "Let's get started",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
